18a4bf9016d4aa9b6eec79bb15abbea3eca6d2df
Cameron Kramer <141282849+Cmkramer62@users.noreply.github.com>
2023-11-29 01:41:42 -0800

    Add files via upload

--- ReShrinkGoHome.cs
+++ ReShrinkGoHome.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ReShrinkGoHome : MonoBehaviour {
 
     public GameObject[] cards;
+    public string[] contentTextList;
+    public TextMeshProUGUI tmpText;
 
     public void ShrinkRest(int index) {
+        tmpText.text = contentTextList[index];
         int i = 0;
         foreach(GameObject card in cards) {
             if (i != index) {
