f6818087117ddbd394dd7b9a4c097b97d8c5f5ed
Cameron Kramer <141282849+Cmkramer62@users.noreply.github.com>
2024-01-22 21:04:04 -0800

    Add files via upload

--- Jumpscare2Trigger.cs
+++ Jumpscare2Trigger.cs
@@ -6,10 +6,12 @@ using UnityEngine.Animations;
 public class Jumpscare2Trigger : MonoBehaviour {
     public GameObject myself;//the ghoul this is attached to.
     public Transform lookAt;
+    public int deathType = 0;
+
     private void OnTriggerEnter(Collider other) {
         if (other.name == "Player Two") {
             myself.SetActive(false);
-            GameObject.Find("Helper Systems").GetComponent<Jumpscare2>().Jumpscare(lookAt);
+            GameObject.Find("Helper Systems").GetComponent<Jumpscare2>().Jumpscare(lookAt, deathType);
         }
     }
 }
