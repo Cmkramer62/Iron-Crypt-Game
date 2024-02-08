82c322809e1913914e6f57ef8fd96756fe1dec7d
Cameron Kramer <141282849+Cmkramer62@users.noreply.github.com>
2024-02-08 03:01:39 -0800

    Add files via upload

---
+++ HealthStation.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthStation : MonoBehaviour {
+
+    public Animator ArmAnimator;
+    public AudioSource ArmSource;
+    public AudioClip TwinkleClip;
+    public GameObject forceField;
+
+    public bool canBeBroken = true;
+
+    private bool onCooldown = false;
+    //OnStart, roll 50/50 chance for broken
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void ActivateMachine() {
+        if (!onCooldown) {
+            forceField.SetActive(true);
+            ArmAnimator.Play("HealthStationArmAnim");
+            ArmSource.Play();
+            StartCoroutine(AnimTimer());
+        }
+    }
+
+    private IEnumerator AnimTimer() {
+        onCooldown = true;
+        yield return new WaitForSeconds(10f);
+        ArmSource.Stop();
+        forceField.SetActive(false);
+        gameObject.GetComponent<AudioSource>().PlayOneShot(TwinkleClip, .2f);
+        GameObject.Find("Player Two").GetComponent<HealthSystem>().RaiseLife();
+        onCooldown = false;
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        ActivateMachine();
+    }
+
+}
