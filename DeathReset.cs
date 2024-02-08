70bf5ea3526ff86cfed58567b26efa57aba3844e
Cameron Kramer <141282849+Cmkramer62@users.noreply.github.com>
2024-02-07 19:56:53 -0800

    Add files via upload

--- DeathReset.cs
+++ DeathReset.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using UnityEngine.SceneManagement;
 using UnityEngine;
 using UnityEngine.Animations;
+using UnityEngine.UI;
 
 public class DeathReset : MonoBehaviour {
     public GameObject player;
@@ -10,12 +11,40 @@ public class DeathReset : MonoBehaviour {
     public Animator fadeAnimator;
     public int nextLevel = 0;
 
+    public GameObject loadingScreen;
+    public Slider slider;
+
+    public AudioSource loadingSource;
+
+    public bool useSimpleNullify = false;
+
+    public bool lowerVolume, raiseVolume = false;
+    private float t = 0;
+    private float g = 0;
+
+    public void Update() {
+        if (lowerVolume) {
+            gameObject.GetComponent<GameSettings>().SetVolume(Mathf.Lerp(0, -80, t));
+            t += .3f * Time.deltaTime;
+        } else if (raiseVolume) {
+            gameObject.GetComponent<GameSettings>().SetVolume(Mathf.Lerp(-80, 0, g));
+            g += .7f * Time.deltaTime;
+            bool result = gameObject.GetComponent<GameSettings>().audioMixer.GetFloat("TestVolume", out float temp);
+            if (result && temp >= 0){
+                raiseVolume = false;
+                g = 0;
+            }
+        }
+    }
+
+
     #region HELPER METHODS
     /*
      * Freeze the player and the camera.
      */
     private void FreezeCharacter() {
-        PlayerNullify.HardSetDisallow();
+        if (!useSimpleNullify) PlayerNullify.HardSetDisallow();
+        else PlayerNullify.SimpleHardSetDisallow();
     }
 
     private void FadeToBlack() {
@@ -25,7 +54,8 @@ public class DeathReset : MonoBehaviour {
 
     private void DeathEffect(int effectNumber) {
         if (effectNumber == 0) { // Hard Reset.
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            StartCoroutine(LoadAsynchronously(SceneManager.GetActiveScene().buildIndex));
+            //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         } else if (effectNumber == 1) {// Soft Reset.
             // do some magic. reset scene while also teleporting player to the shits...what about items?
             //player.transform.position = softCheckpoint.position;
@@ -34,12 +64,14 @@ public class DeathReset : MonoBehaviour {
             player.transform.position = softCheckpoint.position;
             player.transform.rotation = softCheckpoint.rotation;
         } else if (effectNumber == 3) { // Load Level
-            SceneManager.LoadScene(nextLevel);
+            //SceneManager.LoadScene(nextLevel);
+            StartCoroutine(LoadAsynchronously(nextLevel));
         }
     }
 
     private void UnfreezeCharacter() {
-        PlayerNullify.HardSetAllow();
+        if (!useSimpleNullify) PlayerNullify.HardSetAllow();
+        else PlayerNullify.SimpleHardSetAllow();
     }
 
     private void UnfadeFromBlack() {
@@ -48,6 +80,10 @@ public class DeathReset : MonoBehaviour {
     }
     #endregion
 
+    public void StopLoadingSource() {
+        loadingSource.gameObject.SetActive(false);
+    }
+
     public void DeathSequenceStart(int deathType) {
         StartCoroutine(DeathSequence(deathType));
     }
@@ -55,9 +91,38 @@ public class DeathReset : MonoBehaviour {
     private IEnumerator DeathSequence(int deathType) {
         FreezeCharacter();
         FadeToBlack();
+        lowerVolume = true;
         yield return new WaitForSeconds(3f);
+        gameObject.GetComponent<GameSettings>().SetVolume(-80);
+        loadingSource.gameObject.SetActive(true);
+        lowerVolume = false;
+        t = 0;
+        GameObject.Find("Player Two").transform.position = new Vector3(500, 500, 500);
+
+        GameObject MC = GameObject.Find("Main Camera");
+        MC.GetComponent<MouseLook>().allowedToLook = false;
+        MC.GetComponent<MouseLook>().xRotation = 0f;
+
         DeathEffect(deathType);
         UnfreezeCharacter();
+        if (deathType == 2) UnfadeFromBlack();
+    }
+
+    IEnumerator LoadAsynchronously(int sceneIndex) {
+        
+        loadingScreen.SetActive(true);
+
+        yield return new WaitForSeconds(4f);
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+
+        while (!operation.isDone) {
+
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+            slider.value = progress;
+            yield return null;
+        }
+        loadingScreen.SetActive(false);
+        
         UnfadeFromBlack();
     }
 
